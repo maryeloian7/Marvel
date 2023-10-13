561211689b56a5baf7977098d7c9b94476d640e7
Maryia <maritsydik@gmail.com>
2023-10-13 23:12:24 +0200

    Feat: displaying a list of characters

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -7,10 +7,6 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 import MarvelService from "../../services/MarvelServies";
 
 class RandomChar extends Component {
-  constructor(props) {
-    super(props);
-    this.upbateChar();
-  }
   state = {
     char: {},
     loading: true,
@@ -18,6 +14,15 @@ class RandomChar extends Component {
   };
 
   marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.updateChar();
+    // this.timerId = setInterval(this.updateChar, 3000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerId);
+  }
 
   onCharLoaded = (char) => {
     this.setState({ 
@@ -33,7 +38,7 @@ class RandomChar extends Component {
     });
   }
 
-  upbateChar = () => {
+  updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
     this.marvelService
         .getCharacter(id)
@@ -71,10 +76,13 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
-
+    let imgStyle = {'object-fit': 'cover'}
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'object-fit': 'contain'}
+    }
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
