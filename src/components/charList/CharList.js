ece035c79c98350712b2c7aca4c095637d3a04e0
Maryia <maritsydik@gmail.com>
2023-10-16 14:23:20 +0200

    Feat: added error boundary

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,8 @@
 import { Component } from "react";
 
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
 import MarvelService from "../../services/MarvelServies";
 
 import "./charList.scss";
@@ -35,24 +38,29 @@ class CharList extends Component {
   };
 
   render() {
-    const { charList } = this.state;
+    const { charList, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
 
     return (
       <div className="char__list">
+        {errorMessage}
+        {spinner}
         <ul className="char__grid">
           {charList.map((item) => {
-            let imgStyle = { "objectFit": "cover" };
+            let imgStyle = { objectFit: "cover" };
             if (
               item.thumbnail ===
               "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
             ) {
-              imgStyle = { "objectFit": "contain" };
+              imgStyle = { objectFit: "contain" };
             }
             return (
-              <li 
-              className="char__item" 
-              key={item.id} 
-              onClick={() => this.props.onCharSelected(item.id)}>
+              <li
+                className="char__item"
+                key={item.id}
+                onClick={() => this.props.onCharSelected(item.id)}
+              >
                 <img src={item.thumbnail} alt="abyss" style={imgStyle} />
                 <div className="char__name">{item.name}</div>
               </li>
